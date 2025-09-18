51dce697a0419a58860c3b5d32d3cccf6bb39c68
Piyush75812 <piyushkumar63072@gmail.com>
2025-09-18 21:04:18 +0530

    Add files via upload

---
+++ LengthOfLastWord.java
@@ -0,0 +1,20 @@
+class LengthOfLastWord{
+    public static void main(String args[]){
+        String str = "Welcome to my profile";
+        System.out.println(LengthOfLastWord(str));
+    }
+    public static int LengthOfLastWord(String s){
+        String str = s.trim();
+        int count=0;
+        for(int i=str.length()-1;i>=0;i--){
+            if(str.charAt(i)!=' '){
+                count++;
+            }
+            else{
+                break;
+            }
+              
+        }
+        return count;
+    }
+}
