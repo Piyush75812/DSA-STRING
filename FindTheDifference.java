e641e0c7776bbc6a97477a974bbe4c7a1d0b3791
Piyush75812 <piyushkumar63072@gmail.com>
2025-09-20 00:03:04 +0530

    Add files via upload

---
+++ FindTheDifference.java
@@ -0,0 +1,18 @@
+class FindTheDifference{
+    public static void main(String[] args){
+            String s = "abcd";
+            String t = "abcde";
+            System.out.println(findthedifference(s,t));
+        }   
+    public static char findthedifference(String s,String t){
+        int total=0;
+       for(int i=0;i<t.length();i++){
+          total=total+t.charAt(i);
+       }
+       for(int i=0;i<s.length();i++){
+        total = total-s.charAt(i);
+       }
+       return (char)total;
+    }
+
+}
